d963154a5a0b6e6a06e4716934aedebcdaf573d0
juliaisasti <144235263+juliaisasti@users.noreply.github.com>
2024-07-02 16:29:32 +0200

    minor fixes in hero

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,13 +1,13 @@
 const Footer = () => {
   return (
     <>
-    <footer className="p-4 bg-white sm:p-6 dark:bg-gray-800">
+    <footer className="p-4 bg-white sm:p-6 dark:bg-[#031108]">
     <div className="mx-auto max-w-screen-xl">
         <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
-                <a href="https://flowbite.com" className="flex items-center">
-                    <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-8" alt="FlowBite Logo" />
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                <a href="https://juliaisasti.netlify.com" className="flex items-center">
+                    <img src="/favicon.svg" className="mr-3 h-8" alt="Julia Isasti Logo" />
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Julia Isasti</span>
                 </a>
             </div>
             <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
@@ -15,32 +15,29 @@ const Footer = () => {
                     <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
                     <ul className="text-gray-600 dark:text-gray-400">
                         <li className="mb-4">
-                            <a href="https://flowbite.com" className="hover:underline">Flowbite</a>
+                            <a href="https://juliaisasti.netlify.com" className="hover:underline">Personal Portfolio</a>
                         </li>
                         <li>
-                            <a href="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</a>
+                            <a href="https://github.com/juliaisasti/spotify-dj" className="hover:underline">Project's repository</a>
                         </li>
                     </ul>
                 </div>
                 <div>
-                    <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
+                    <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow me</h2>
                     <ul className="text-gray-600 dark:text-gray-400">
                         <li className="mb-4">
-                            <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Github</a>
+                            <a href="https://github.com/juliaisasti" className="hover:underline ">Github Profile</a>
                         </li>
                         <li>
-                            <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Discord</a>
+                            <a href="https://www.linkedin.com/in/juliaisasti/" className="hover:underline">LinkedIn</a>
                         </li>
                     </ul>
                 </div>
                 <div>
-                    <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
+                    <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Other links</h2>
                     <ul className="text-gray-600 dark:text-gray-400">
                         <li className="mb-4">
-                            <a href="#" className="hover:underline">Privacy Policy</a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:underline">Terms &amp; Conditions</a>
+                            <a href="https://developer.spotify.com/documentation/web-api" className="hover:underline">Spotify's Web API</a>
                         </li>
                     </ul>
                 </div>
@@ -48,7 +45,7 @@ const Footer = () => {
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://juliaisasti.netlify.com" className="hover:underline">Julia Isasti</a>. All Rights Reserved.
             </span>
             <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
                 <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
