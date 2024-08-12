338539ea6bbbc99b18522affd74355d658fc0454
juliaisasti <144235263+juliaisasti@users.noreply.github.com>
2024-08-12 17:05:24 +0200

    deployed on render

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://spotifybpm-beta.vercel.app/',
+        target: 'https://spotifybpm-axi2.onrender.com/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
