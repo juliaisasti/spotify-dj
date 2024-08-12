a5f2f7301595d27c1165fe34c99b22f4ea26b298
juliaisasti <144235263+juliaisasti@users.noreply.github.com>
2024-08-12 16:04:03 +0200

    env changed

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://spotifybpm-beta.vercel.app/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
